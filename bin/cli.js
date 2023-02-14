a5b34db271b95d6895d4e1b9c35ed9259e8b7a21
coy_liu <chao_liu@fengzhiding.com>
2023-02-14 16:40:17 +0800

    全面化优化

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const { program } = require("commander");
 const chalk = require("chalk");
-// const ora = require("ora");
+const ora = require("ora");
 
 program
   .version("0.1.0")
@@ -13,31 +13,31 @@ program
     // 文本样式
     console.log("project name is " + chalk.bold(name));
 
-    // // 自定义文本信息
-    // const message = "Loading unicorns";
-    // // 初始化
-    // const spinner = ora(message);
-    // // 开始加载动画
-    // spinner.start();
+    // 自定义文本信息
+    const message = "Loading unicorns";
+    // 初始化
+    const spinner = ora(message);
+    // 开始加载动画
+    spinner.start();
 
-    // setTimeout(() => {
-    //   // 修改动画样式
+    setTimeout(() => {
+      // 修改动画样式
 
-    //   // Type: string
-    //   // Default: 'cyan'
-    //   // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
-    //   spinner.color = "red";
-    //   spinner.text = "Loading rainbows";
+      // Type: string
+      // Default: 'cyan'
+      // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
+      spinner.color = "red";
+      spinner.text = "Loading rainbows";
 
-    //   setTimeout(() => {
-    //     // 加载状态修改
-    //     spinner.stop(); // 停止
-    //     spinner.succeed("Loading succeed"); // 成功 ✔
-    //     // spinner.fail(text?);  失败 ✖
-    //     // spinner.warn(text?);  提示 ⚠
-    //     // spinner.info(text?);  信息 ℹ
-    //   }, 2000);
-    // }, 2000);
+      setTimeout(() => {
+        // 加载状态修改
+        spinner.stop(); // 停止
+        spinner.succeed("Loading succeed"); // 成功 ✔
+        // spinner.fail(text?);  失败 ✖
+        // spinner.warn(text?);  提示 ⚠
+        // spinner.info(text?);  信息 ℹ
+      }, 2000);
+    }, 2000);
   });
 
 program.parse();
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #! /usr/bin/env node
+
 const { program } = require("commander"); // 引入
-// const { chooseTemplate } = require("./inquirers.js");
+const download = require("download-git-repo");
+const templateMap = require("./src/templateMap");
+const ora = require("ora");
+const chalk = require("chalk");
+const fs = require("fs");
+
+const { modifyPackage } = require("./src/modifyFiles");
+
+const { chooseTemplate } = require("./src/inquirers");
+
 function start() {
+  console.log(chalk.rgb(216, 27, 96)("\n 😈😈😈  雷猴啊, 靓仔~~"));
+  console.log(
+    chalk.cyanBright(" 🦄🦄🦄  靓仔正在使用channing-cli命令行工具...\n")
+  );
+
   program.version(require("./package.json").version); // 输出版对应的版本号
+
   program
     .command("create <projectName>")
     .description("用于创建一个项目模板")
@@ -10,22 +26,69 @@ function start() {
     .action(async function (projectName, options) {
       let template = options.template;
       projectName = projectName || "untitled";
-      //   if (!template) {
-      //     template = await chooseTemplate();
-      //   }
-    //   console.log("chooseTemplate :>> ", chooseTemplate);
-      console.log(`成功创建项目：${projectName}`);
-      console.log(`所使用的模板：${template}`);
+
+      if (!template) {
+        template = await chooseTemplate(); // 注意这里是一个异步方法
+      }
+
+      console.log(
+        chalk.rgb(69, 39, 160)("你选择的模板是 👉"),
+        chalk.bgRgb(69, 39, 160)(template)
+      );
+
+      // 下载前提示loading
+      const spinner = ora({
+        text: "正在下载模板...",
+        color: "yellow",
+        spinner: {
+          interval: 80,
+          frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"],
+        },
+      });
+      spinner.start();
+
+      /**
+       * @downloadUrl   注意所需要的格式，不要直接复制粘贴仓库地址
+       *
+       * @project       项目名称
+       *
+       */
+      const downloadUrl = templateMap.get(template);
+      download(downloadUrl, projectName, { clone: true }, async (error) => {
+        if (error) {
+          spinner.fail(`下载失败 😭😭😭`);
+          console.log(
+            chalk.bgRgb(220, 0, 8)(`  创建项目失败：${projectName} `),
+            "😭😭😭"
+          );
+          console.log(
+            "🧐🧐🧐 失败原因：",
+            chalk.bgRgb(220, 0, 8)(error.message)
+          );
+        } else {
+          spinner.succeed(`下载完成：${projectName}`);
+
+          await modifyPackage(projectName);
+
+          console.log(
+            "✌✌✌",
+            chalk.rgb(69, 39, 160)("成功创建项目  👉  "),
+            chalk.bgRgb(69, 39, 160)(projectName)
+          );
+        }
+      });
     });
+
   program
     .command("checkAll")
     .description("查看所有的模板")
     .action(function () {
       const templateList = ["vue-default-template", "vue-default-template-ts"];
       templateList.forEach((temp, index) => {
-        console.log(`(${index + 1})  ${temp}`);
+        console.log(chalk.rgb(69, 39, 160)(`(${index + 1})  ${temp}`));
       });
     });
+
   program.parse(process.argv);
 }
 
